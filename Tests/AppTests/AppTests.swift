653552f09577c44c39397878570f435370d334a9
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-11-27 09:01:17 +0200

    add test for pokemon endpoint

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -2,14 +2,26 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
+
+    func testPokemon() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        try app.test(.GET, "pokemon") { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "This is the Pokémon endpoint")
+        }
+    }
+
     func testHelloWorld() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
 
-        try app.test(.GET, "hello", afterResponse: { res in
+        try app.test(.GET, "hello") { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "Hello, world!")
-        })
+        }
     }
 }
